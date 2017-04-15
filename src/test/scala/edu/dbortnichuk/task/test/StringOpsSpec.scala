748574e4bdf11e0004ff12d9d6c95875c02517b3
dbortnichuk <dbortnichuk@gmail.com>
2017-04-15 21:00:58 +0300

    string ops: split

--- src/test/scala/edu/dbortnichuk/task/test/StringOpsSpec.scala
+++ src/test/scala/edu/dbortnichuk/task/test/StringOpsSpec.scala
@@ -7,8 +7,20 @@ import org.specs2.mutable.Specification
 class StringOpsSpec extends Specification {
 
   "String Ops" should {
-    "split correctly" in {
+    "split correctly as per task" in {
       StringOps.split(by = "+", "-")(of = "3.8 - 49 + 7") == List("3.8 ", "-", " 49 ", "+", " 7")
+    }
+
+    "split correctly with single delim" in {
+      StringOps.split(by = "aa")(of = "aabbacaadd") == List("aa", "bbac", "aa", "dd")
+    }
+
+    "split correctly with multi delim" in {
+      StringOps.split(by = "aa", "a")(of = "aabbacaadd") == List("aa", "bb", "a", "c", "aa", "dd")
+    }
+
+    "split correctly with multi delim prioritised" in {
+      StringOps.split(by = "a", "aa")(of = "aabbacaadd") == List("a", "a", "bb", "a", "c", "a", "a", "dd")
     }
 
   }
